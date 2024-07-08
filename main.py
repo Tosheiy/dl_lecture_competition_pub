c295ae9f6cb701826b719039b9c809facae59f52
Windows AI <asf42322@gmail.com>
2024-07-09 00:35:25 +0900

    first check

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ def run(args: DictConfig):
     loader_args = {"batch_size": args.batch_size, "num_workers": args.num_workers}
     
     train_set = ThingsMEGDataset("train", args.data_dir)
+    # ThingsMEGDatasetの__itemget__()関数が呼び出される
     train_loader = torch.utils.data.DataLoader(train_set, shuffle=True, **loader_args)
     val_set = ThingsMEGDataset("val", args.data_dir)
     val_loader = torch.utils.data.DataLoader(val_set, shuffle=False, **loader_args)
